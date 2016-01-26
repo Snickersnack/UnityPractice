3cc04adef8751a9a456a980a973b2fb49e84d503
Wilson Tan <wilsontan46@gmail.com>
2016-01-25 22:58:15 -0800

    Fiddling with Unity

---
+++ Assets/Playercontroller.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Playercontroller : MonoBehaviour {
+	public float movementSpeed = 10;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position+= new Vector3(Input.GetAxis("Horizontal"),0,0);
+	}
+}
